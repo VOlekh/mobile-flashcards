2b42e9485ffc9e362978d1d7dd4334cfab5193f5
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-16 12:57:21 +0200

    add actons

--- actions/index.js
+++ actions/index.js
@@ -1,18 +1,43 @@
-export const RECEIVE_ENTRIES = 'RECEIVE_ENTRIES'
-export const ADD_ENTRY = 'ADD_ENTRY'
+export const RECEIVE_DECKS = 'RECEIVE_DECKS'
+export const ADD_DECK = "ADD_DECK";
+export const ADD_CARD = "ADD_CARD";
+export const DELETE_DECK = "DELETE_DECK";
+export const DELETE_CARD = "DELETE_CARD";
 
-export function receiveEntries (entries) {
-    //console.log("receiveEntry")
+export function receiveDecks (decks) {
+    //console.log("receiveDecks")
   return {
-    type: RECEIVE_ENTRIES,
-    entries,
+    type: RECEIVE_DECKSS,
+    decks,
   }
 }
 
-export function addEntry (entry) {
-    //console.log("addEntry")
-  return {
-    type: ADD_ENTRY,
-    entry,
+export function addDeck(deck) {
+    return {
+      type: ADD_DECK,
+      deck,
+    };
   }
-}
+  
+  export function addCard(deckTitle, question) {
+    return {
+      type: ADD_CARD,
+      deckTitle,
+      question,
+    };
+  }
+  
+  export function deleteDeck(deckTitle) {
+    return {
+      type: DELETE_DECK,
+      deckTitle,
+    };
+  }
+  
+  export function deleteCard(deckTitle, question) {
+    return {
+      type: DELETE_CARD,
+      deckTitle,
+      question,
+    };
+  }
