01529229424bdf3450042c9a70355b94c2ef029a
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-19 13:36:18 +0200

    style home page

--- App.js
+++ App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import {StyleSheet,  Text,  View,   Platform, TouchableOpacity, StatusBar} from "react-native";
-// import AddDeck from "./components/AddDeck";
-// import DecksList from "./components/DecksList";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import middleware from "./middleware";
 import Button from 'react-native-button';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
 // import { NavigationContainer } from "@react-navigation/native";
-//import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 // import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 // import { createStackNavigator } from "@react-navigation/stack";
 // import Deck from "./components/IndividualDeck";
@@ -20,67 +18,10 @@ import { Ionicons } from '@expo/vector-icons';
 // import { FontAwesome, Entypo } from '@expo/vector-icons';
 // import { setLocalNotification } from './utils/helpers';
 // import Constants from "expo-constants";
-import { lightBlue, white } from './utilits/colors'
-
-
-// function UdaciStatusBar({ backgroundColor, ...props }) {
-//   return (
-//     <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
-//       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
-//     </View>
-//   );
-// }
-
-// const Tab =
-//   Platform.OS === "ios"
-//     ? createBottomTabNavigator()
-//     : createMaterialTopTabNavigator();
-
-// function Tabs() {
-//   return (
-//     <NavigationContainer>
-//       <Tab.Navigator
-//         initialRouteName="Decks List"
-//         tabBarOptions={{
-//           labelPosition: "below-icon",
-//           activeTintColor: Platform.OS === "ios" ? purple : white,
-//           style: {
-//             height: 56,
-//             backgroundColor: Platform.OS === "ios" ? white : purple,
-//             shadowColor: "rgba(0, 0, 0, 0.24)",
-//             shadowOffset: {
-//               width: 0,
-//               height: 3,
-//             },
-//             shadowRadius: 6,
-//             shadowOpacity: 1,
-//           }
-//         }}
-//       >
-//         <Tab.Screen
-//           name="Decks List"
-//           component={DecksList}
-//           options={{
-//             tabBarLabel: "Decks List",
-//             tabBarIcon: ({ color }) => (
-//               <FontAwesome name="plus-square" size={30} color={color} />
-//             )
-//           }}/>
-
-//         <Tab.Screen
-//           name="Add Deck"
-//           component={AddDeck}
-//           options={{
-//             tabBarLabel: "Add Deck",
-//             tabBarIcon: ({ color }) => (
-//               <Entypo name="add-to-list" size={30} color={color} />
-//             )
-//           }}
-//           />
-//       </Tab.Navigator>
-//      </NavigationContainer>
-//   );
-// }
+// import { lightBlue, white } from './utilits/colors'
+// import AddDeck from "./components/AddDeck";
+// import DecksList from "./components/DecksList";
+//<a href='https://pngtree.com/so/black-and-white'>black and white png from pngtree.com</a>
 
 function HomeScreen() {
   return (
@@ -109,14 +50,22 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer, middleware)}>
-        <View styles={styles.container}>
+        <View >
 
+            <View style={styles.appTitleContainer}>
+              <Text style={styles.appTitle}>Cards project</Text>
+            </View>
 
-            <Text styles={styles.appTitle}>Cards project</Text>
-         
+            <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                <View style={styles.line} />
+                <View>
+                <Text style={styles.appTitleSmall}> IT CARDS </Text>
+                </View>
+                <View  style={styles.line} />
+            </View>
 
-            <View styles={styles.bottomTextContainer}>
-              <Text style={styles.bottomText}>A mobile application (Android or iOS - or both) that allows users to study collections of flashcards. The app allow users to create different categories of flashcards called "decks", add flashcards to those decks, then take quizzes on those decks.</Text>
+            <View style={styles.topTextContainer}>
+              <Text style={styles.topText}>Study collections of flashcards, create different categories of flashcards called "decks", add flashcards to those decks, then take quizzes on those decks.</Text>
             </View>
 
             <TouchableOpacity >
@@ -151,7 +100,7 @@ export default class App extends React.Component {
 {/* https://reactnavigation.org/docs/tab-based-navigation */}
 
 
-            <NavigationContainer style={styles.navContainer}>
+            <NavigationContainer style={styles.navConttainer}>
               <Tab.Navigator
                   screenOptions={({ route }) => ({
                     tabBarIcon: ({ focused, color, size }) => {
@@ -193,8 +142,12 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 8,
     marginHorizontal: 16,
-    borderRadius: 5, 
-  
+    borderRadius: 5,
+    alignItems: "center",
+  },
+
+  navConttainer:{
+   
   },
 
   title: {
@@ -208,28 +161,49 @@ const styles = StyleSheet.create({
     color: "#464646",
   },
 
-  appTitle:{
-    fontSize: 60,
-    fontWeight: 'bold',
-    color: "#f0f",
-  },
-
-  bottomTextContainer: {
+  appTitleContainer:{
     padding: 20,
-    marginVertical: 80,
+    marginBottom: 7,
+    marginTop:80,
     marginHorizontal: 16,
     alignItems: "center",
     justifyContent: "center",
+  
+   // backgroundColor: "#40676E", 
+  },
+
+  appTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: "#93B7BE",
+  },
+
+  appTitleSmall: {
+    fontSize: 15,
+    color: "#93B7BE",
+  },
+
+  line:{
+    flex: 1, 
+    height: 0.7, 
+    backgroundColor: "#bfbfbf"
+  },
+
+  topTextContainer: {
+    padding: 20,
+    marginVertical: 20,
+    marginHorizontal: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    
 
   },
 
-  bottomText:{
+  topText:{
     fontSize: 16,
     color: "#464646",
+    fontStyle: "italic",
+    textAlign:  "center",
   },
 
-  
-  navContainer: {
-    backgroundColor: "#93B7BE",
-  },
 });
