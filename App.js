7e8465c4ae6703f721027b6ac67de64bfb433d45
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-24 21:20:18 +0200

    clear up code

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import {StyleSheet,  Button, Text,  View,   Platform, TouchableOpacity, StatusBar} from "react-native";
+import React from "react";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
@@ -8,18 +7,9 @@ import AppNavigation from "./components/AppNavigation";
 //import AppHeader from "./components/Header";
 
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
-// import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-// import Deck from "./components/IndividualDeck";
-// import Quiz from "./components/Quiz";
-// import { FontAwesome, Entypo } from '@expo/vector-icons';
-// import { setLocalNotification } from './utils/helpers';
-// import Constants from "expo-constants";
-// import { lightBlue, white } from './utilits/colors'
-// import AddDeck from "./components/AddDeck";
-// import DecksList from "./components/DecksList";
+
 //<a href='https://pngtree.com/so/black-and-white'>black and white png from pngtree.com</a>
+{/* https://reactnavigation.org/docs/tab-based-navigation */}
 
 
 
@@ -35,77 +25,7 @@ export default class App extends React.Component {
          <NavigationContainer>
             <AppNavigation />
          </NavigationContainer>
-        
-       
-{/* https://reactnavigation.org/docs/tab-based-navigation */}
-
-
-
       </Provider>
     );
   }
 }
-
-
-// -------------Styles------------------------
-const styles = StyleSheet.create({
-  deck: {
-    backgroundColor: "#93B7BE",
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    borderRadius: 5,
-   // alignItems: "center",
-  },
-
-
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: "#fff",
-  },
-
-  count: {
-    fontSize: 16,
-    color: "#464646",
-  },
-
-
- 
-
-  topTextContainer: {
-    padding: 20,
-    marginVertical: 20,
-    marginHorizontal: 16,
-    alignItems: "center",
-    justifyContent: "center",
-    //backgroundColor: "#D5C7BC", 
-    backgroundColor: "#d9d9d9",
-    borderRadius: 5, 
-  },
-
-
-  topText:{
-    fontSize: 16,
-    color: "#464646",
-    fontStyle: "italic",
-    textAlign:  "center",
-  },
-
-  
- button: {
-    padding: 10,
-    marginVertical: 5,
-    marginHorizontal: 16,
-    alignItems: "center",
-    justifyContent: "center",
-    //backgroundColor: "#D5C7BC", 
-    backgroundColor: "#d9d9d9",
-    borderRadius: 5,
-    shadowColor:"#454545",
-  },
-
-
-
-
-});
