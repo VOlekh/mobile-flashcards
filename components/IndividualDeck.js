99b85a29bc4dbeebd2751e78f062702eb853e45b
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-26 21:56:04 +0200

    add mapstateToProps

--- components/IndividualDeck.js
+++ components/IndividualDeck.js
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, Button, TouchableOpacity, StyleSheet} from "react-native";
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
 
-function IndividualDeck({ navigation, route }) {
-    return (
-        <View style={styles.conteiner}>
 
-            <TouchableOpacity>
-                <View style={styles.deck}>
-                    {/* <Text style={styles.title}>{title}</Text> */}
-                    <Text style={styles.title}>Deck 1</Text>
-                    {/* <Text style={styles.count}>{length} cards</Text> */}
-                    <Text style={styles.count}>XX cards</Text>
+class IndividualDeck extends Component  {
+   
+    render(){
+        const { navigation, route, deck } = this.props;
+
+        return (
+            <View style={styles.conteiner}>
+
+                <TouchableOpacity>
+                    <View style={styles.deck}>
+                        {/* <Text style={styles.title}>{title}</Text> */}
+                        <Text style={styles.title}>{deck.title}</Text>
+                        {/* <Text style={styles.count}>{length} cards</Text> */}
+                        <Text style={styles.count}>{deck.questions.length}</Text>
+                    </View>
+                </TouchableOpacity>
+
+                <View style= {styles.button}>
+                    <Button
+                        onPress={() =>
+                            navigation.navigate('Quiz', { name: 'Quiz' })
+                        }
+                        title="Start Quiz"
+                        color= "#464646"
+                    /> 
                 </View>
-            </TouchableOpacity>
-
-            <View style= {styles.button}>
-                <Button
-                    onPress={() =>
-                        navigation.navigate('Quiz', { name: 'Quiz' })
-                    }
-                    title="Start Quiz"
-                    color= "#464646"
-                /> 
-            </View>
-            <View style= {styles.button}>
-                <Button
-                    onPress={() =>
-                        navigation.navigate('AddCard', { name: 'AddCard' })
-                    }
-                    title="Add Card"
-                    color= "#464646"
-                /> 
-            </View>
-            
-      </View>  
-    )
+                <View style= {styles.button}>
+                    <Button
+                        onPress={() =>
+                            navigation.navigate('AddCard', { name: 'AddCard' })
+                        }
+                        title="Add Card"
+                        color= "#464646"
+                    /> 
+                </View>
+                
+        </View>  
+        )
+    };
 }
-export default IndividualDeck;
+
+function mapStateToProps(state, { route, navigation }) {
+    const { deckID } = route.params;
+    return {
+        deck: state[deckID],
+        navigation,
+    };
+}
+
+export default connect(mapStateToProps) (IndividualDeck);
 
 // -------------Styles------------------------
 const styles = StyleSheet.create({
