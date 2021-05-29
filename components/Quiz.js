e87c477a1263f2ef3f28439c6667769dea4eb9db
Valentina-Olekhnovich <volekh@gmail.com>
2021-05-30 00:53:39 +0200

    add counter

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,45 +1,74 @@
-import React from "react";
+import React, { Component } from "react";
+import { render } from "react-dom";
 import { Text, View, Button, TouchableOpacity, StyleSheet} from "react-native";
+import { connect } from "react-redux";
+//https://www.linkedin.com/pulse/how-develop-counter-app-react-native-floating-button-mahmud-ahsan/
 
-function Quiz({ navigation, route }) {
-    return (
-        <View style={styles.conteiner}>
+class Quiz extends Component  {
+    state = { countCorrect: 0}
+    setCountCorrect = () => this.setState(
+    prevState => ({ ...prevState, countCorrect: this.state.countCorrect + 1 })
+  )
+    render(){  
+        const {countCorrect} = this.state;
+        let { navigation, route, deck} = this.props;
+        return (
+            <View style={styles.conteiner}>
 
-            <TouchableOpacity>
-                <View style={styles.deck}>
-                    <Text style={styles.title}>Deck 1</Text>
-                    <Text>TBD: the number of cards left in the quiz</Text>
+                <TouchableOpacity>
+                    <View style={styles.deck}>
+                        <Text style={styles.title}>Deck 1</Text>
+                        <Text>TBD: the number of cards left in the quiz</Text>
+                        <Text >correctCounter: {countCorrect}</Text>
 
-                    <View style={styles.question}>
-                        <Text style={styles.count}>question text text text 111111111111111111111111111111111111111111111111111111111111111111111111111111111111</Text>
+                        <View style={styles.question}>
+                            <Text style={styles.count}>question text text text 111111111111111111111111111111111111111111111111111111111111111111111111111111111111</Text>
+                        </View>
+
+                        <Button
+                            title="Flip"
+                            color= "#464646"
+                        /> 
                     </View>
+                </TouchableOpacity>
 
+                
+{/* TBD: rebuild button with TouchableOpacity */}
+                <View style= {styles.button}>
                     <Button
-                    title="Flip"
-                    color= "#464646"
+                        onPress={this.setCountCorrect}
+                        title="Correct"
+                        color= "#464646" 
                     /> 
+                    
                 </View>
-            </TouchableOpacity>
 
-            <View style= {styles.button}>
-                <Button
-                    title="Correct"
-                    color= "#464646"
-                /> 
-                 
-            </View>
+                <View style= {styles.buttonIncorrect}>
+                    <Button
+                        title="Incorrect"
+                        color= "#464646"
+                    /> 
+                </View>    
+                
+        </View>  
+        )
+    }
 
-            <View style= {styles.buttonIncorrect}>
-                <Button
-                    title="Incorrect"
-                    color= "#464646"
-                /> 
-             </View>    
-            
-      </View>  
-    )
+    onCorrect(){
+        this.setState({counter: this.state.counter + 1});
+    }
+} 
+
+function mapStateToProps(state, { route, navigation }) {
+    const { id } = route.params;
+    return {
+        deck: state[id],
+        //questions: state[id].questions,
+        navigation,
+    };
 }
-export default Quiz;
+  
+export default connect(mapStateToProps)(Quiz);
 
 // -------------Styles------------------------
 const styles = StyleSheet.create({
