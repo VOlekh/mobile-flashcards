61fcdb4ae9114f314bfd57c8f1ee8b25adac65cd
Valentina-Olekhnovich <volekh@gmail.com>
2021-06-06 15:41:30 +0200

    Add more cards for initial upload

--- utilits/api.js
+++ utilits/api.js
@@ -25,6 +25,46 @@ export const dummy_initial_data = {
       },
     ],
   },
+  HTML: {
+    title: "HTML",
+    questions: [
+      {
+        question: "What is HTML?",
+        answer:
+          "HTML stands for Hyper Text Markup Language. It is a language of the World Wide Web. It is a standard text formatting language which is used to create and display pages on the Web. HTML makes the text more interactive and dynamic. It can turn text into images, tables, links. HTML pages are saved by adding .html or .html in web page name.",
+      },
+      {
+        question: " What are Attributes and how do you use them?",
+        answer:
+          "Attributes are specified directly after the name of the tag, inside the two angled brackets. They should only ever appear in opening tags or in self-closing tags. But, they can never be in closing tags.",
+      },
+      {
+        question: "What is the difference between DIV and SPAN in HTML?",
+        answer:
+          "The difference between span and div is that a span element is in-line and usually used for a small chunk of HTML inside a line,such as inside a paragraph. Whereas, a div or division element is block-line which is equivalent to having a line-break before and after it and used to group larger chunks of code."
+      },
+    ],
+  },
+  ReactNative: {
+    title: "React Native",
+    questions: [
+      {
+        question: "Can we combine Android or iOS code in react native?",
+        answer:
+          "Yes, we can combine Android or iOS code in react native. ",
+      },
+      {
+        question: "What is the difference between React and React Native?",
+        answer:
+          "React is a JavaScript library, supporting front-end web and being run on a server, for web applications and building user interfaces.	React Native is a framework that compiles to native app components, allowing you to build native mobile applications for different platforms.	React Js is a Javascript Library where you can develop and run faster web applications.	React-Native is a framework where you can develop mobile applications.	React is for websites.	React Native is for mobile applications. ",
+      },
+      {
+        question: "What are the advantages of using React Native?",
+        answer:
+          " Conveniently uses the client as well as server side. Superbly cost effective and code reuse.  Better code readability because of JSX use.  Easy to integrate with other significant frameworks. Easy to write UI test cases because of React"
+      },
+    ],
+  },
 };
 
 const CARDS_STORAGE_KEY = "MobileFlashcards:decks";
